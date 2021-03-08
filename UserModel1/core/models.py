f6d97f502e236a229ea93dcf747650d9f45227d6
jhmiante <jhmiante@gmail.com>
2021-03-07 22:41:48 -0300

    Utilizando o User Modo 1
    Não recomendado

--- UserModel1/core/models.py
+++ UserModel1/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+#Não é a forma ideal
+class Post(models.Model):
+    autor = models.CharField(User, verbose_name='Autor', on_delete=models.CASCADE)
+    titulo = models.CharField('Titulo', max_length=100)
+    text = models.CharField('Texto', max_length=400)
+
+    def __str__(self):
+        return self.titulo
