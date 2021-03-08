0095885ca94bc0966d93ac87eb6d7ffe614881a3
jhmiante <jhmiante@gmail.com>
2021-03-07 23:04:34 -0300

    Filtrando dados pelo Usuário Logado

--- UserModel1/core/admin.py
+++ UserModel1/core/admin.py
@@ -5,4 +5,11 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'autor')
+    list_display = ('titulo', '_autor')
+
+    def _autor(self, intance):
+        return f'{intance.autor.get_full_name()}'
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(autor=request.user)
