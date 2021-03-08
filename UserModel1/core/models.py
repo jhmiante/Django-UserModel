899317846ae60ed39fc890707028f82f690169b4
jhmiante <jhmiante@gmail.com>
2021-03-07 22:54:55 -0300

    Corrigindo Models e Registrando no ADM

--- UserModel1/core/models.py
+++ UserModel1/core/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-#Não é a forma ideal
 class Post(models.Model):
-    autor = models.CharField(get_user_model(), verbose_name='Autor', on_delete=models.CASCADE)
+    autor = models.ForeignKey(get_user_model(), verbose_name='Autor', on_delete=models.CASCADE)
     titulo = models.CharField('Titulo', max_length=100)
     text = models.CharField('Texto', max_length=400)
 
